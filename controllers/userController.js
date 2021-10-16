e80f71f1d0093a30d520cfc34c27db5d6c5b53ec
bryancurtis <curtispro97@gmail.com>
2021-10-16 15:03:40 -0700

    adds resposnive image capability for main banner

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,22 @@
 
 const express = require("express"); 
 const router = express.Router();
-const {} = require("../models");
+const { User } = require("../models");
+
+
+
+
+// Profile show
+router.get("/:username", function (req, res, next) {
+    try {
+        const user = User.find({ username: req.params.username });
+        // stopped here, continue later when I cant test with views
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
+
 
 module.exports = router;
