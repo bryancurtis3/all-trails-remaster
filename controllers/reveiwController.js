3c549f01357e97888dbadfe7abf519b554f5d2f0
bryancurtis <curtispro97@gmail.com>
2021-10-18 16:47:23 -0700

    outlines review create route and revises modal to work with submitting the form

--- controllers/reveiwController.js
+++ controllers/reveiwController.js
@@ -2,6 +2,22 @@
 
 const express = require("express"); 
 const router = express.Router();
-const {User, Trail } = require("../models");
+const {User, Trail, Review } = require("../models");
+
+// base URL === /:id
+
+router.post("/reviews", function (req, res, next) {
+    req.body.trail = req.params.id; 
+    console.log(req.body.trial);
+    Review.create(req.body, function (error, newReview) {
+        try {
+            res.redirect("/:id")
+        } catch (error) {
+            console.log(error);
+            req.error = error;
+            next();
+        }
+    })
+});
 
 module.exports = router;
