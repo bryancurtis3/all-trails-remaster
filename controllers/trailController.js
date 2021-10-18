93a1b7033f45600e42ff4872c2a8fc5774c69060
bryancurtis <curtispro97@gmail.com>
2021-10-17 19:43:01 -0700

    adds start of trais show page and routes to the show pages

--- controllers/trailController.js
+++ controllers/trailController.js
@@ -6,12 +6,27 @@ const { Trail } = require("../models");
 
 //== base route /trails 
 
+// Trails Index
 router.get("/", async function (req, res, next) {
     try {
         const allTrails = await Trail.find({});
         const context = { trails: allTrails };
 
         return res.render("trails/index", context);
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        next();
+    }
+});
+
+// Trail Show
+router.get("/:id", async function (req, res, next) {
+    try {
+        const trail = await Trail.findById(req.params.id);
+        const context = { trail: trail };
+        
+        return res.render("trails/show", context);
     } catch (error) {
         console.log(error);
         req.error = error;
