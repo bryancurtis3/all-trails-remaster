2fd3c8b291fda33cd7668cc63c821f5a49f8903d
alexwill44 <AlArvanitis@gmail.com>
2021-10-18 21:35:46 -0700

    created list view associated with user

--- controllers/listController.js
+++ controllers/listController.js
@@ -13,12 +13,17 @@ const { User, Trail, List } = require("../models");
 router.get("/lists", async function(req, res, next){
     try{
         const userList = await List.find( {user_id: req.session.currentUser.id} ).sort("-createdAt");
+            for (i = 0; i < userList.length; i++ ) {
+                const firstTrail = await Trail.findById( `${userList[i].trail_id[0]}`);
+                await console.log(firstTrail);
+                userList[i].image = await firstTrail.image; 
+                await console.log(firstTrail.image);
+        };
         const authUser = await User.findById(req.session.currentUser.id);
         const context = {
             lists: userList,
             user: authUser,
         };
-        await console.log(`log======== ${userList}`);
 
         return res.render ("lists/index", context);
     }
